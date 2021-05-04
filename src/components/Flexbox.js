1c8e692ef8beb9cd00f7aeebbbab21dab84672ca
Calli Oberembt <croberembt@gmail.com>
2021-05-03 21:50:55 -0500

    practicing css flexbox

---
+++ src/components/Flexbox.js
@@ -0,0 +1,37 @@
+import React from 'react'; 
+import './Flexbox.css'; 
+
+const Flexbox = () => {
+  return (
+    <div className='flexbox'>
+      <div className='example-1'>
+        <div className='box box-1'>
+          1
+        </div>
+        <div className='box box-2'>
+          2
+        </div>
+        <div className='box box-3'>
+          3
+        </div>
+        <div className='box box-4'>
+          4
+        </div>
+        <div className='box box-5'>
+          5
+        </div>
+        <div className='box box-6'>
+          6
+        </div>
+        <div className='box box-7'>
+          7
+        </div>
+        <div className='box box-8'>
+          8
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Flexbox;
