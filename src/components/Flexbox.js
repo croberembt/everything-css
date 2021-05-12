e200a0956ace3181b344c1873cb5dc0a866bc4ff
Calli Oberembt <croberembt@gmail.com>
2021-05-11 19:45:55 -0500

    continued playing arount with css flexbox

--- src/components/Flexbox.js
+++ src/components/Flexbox.js
@@ -7,34 +7,34 @@ const Flexbox = () => {
       <div className='box-container'>
         <div className='box box-1'>
           <div className='box-text'>
-              <div>
+              <div style={{border: "solid 1px black"}}>
                 1asdfsdfsdfasdffffffffffffffffffffffffffafsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
               </div>
           </div>
         </div>
         <div className='box box-2'>
-          <div className='box-text'>1asdfsdfsdfasdffffffffffffffffffffffffffdafsssssssssssssssssssssssssssssssssssssssssss</div>
+          <div className='box-text'>1asdfsdfsdf asdfffffffffffffffff fffffffffdafsssssssssssssss ssssssssss ssssssssssssss ssss</div>
         </div>
         <div className='box box-3'>
-          <div className='box-text'>1asdfsdfsdfasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffff</div>
+          <div className='box-text'>1asdfsdfsdfasdffffff fffffffffffffffffffffff fffffff fffffffffff ffffffffff</div>
         </div>
         <div className='box box-4'>
-          <div className='box-text'>1asdfsdfsdfasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffff</div>
+          <div className='box-text'>1asdfsdfsdfasd fffffffffffffffffffffff fff fffffffffff fffffffff fffffffffff</div>
         </div>
         <div className='box box-5'>
-          <div className='box-text'>1asdfsdfsdfasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffff</div>
+          <div className='box-text'>1 asdfsdfsdfasdfffffffffffffffff fffffffffffffffff fffffffff fffffffff fffff</div>
         </div>
         <div className='box box-6'>
-          <div className='box-text'>1asdfsdfsdfasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffff</div>
+          <div className='box-text'>1asdfsdfs dfasdffffffffff ffffffffffff fffffffffff ffffffffff fffffffff fffff</div>
         </div>
         <div className='box box-7'>
-          <div className='box-text'>1asdfsdfsdfasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffff</div>
+          <div className='box-text'>1asdfsdfsdfa sdfffffffffffff fffffff fffffffffffffff fffffffffff fffffffffff</div>
         </div>
         <div className='box box-8'>
-          <div className='box-text'>1asdfsdfsdfasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffff</div>
-        </div>
+          <div className='box-text'>1asdfsdf sdfasdffffffffff ffffffffffffffffff fffffff ffffffff fff fffffffffff</div>
+        </div> 
         <div className='box box-9'>
-          <div className='box-text'>1asdfsdfsdfasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffff</div>
+          <div className='box-text'>1asdfsdfsdf asdfffffffff ffffffffffffff fffffffffffffff fff fffffffffffffff</div>
         </div>
       </div>
     </div>
